8ba1154203de09258cd75d51c17537b3962f9992
Nithin Chandran P <p.nithinc@gmail.com>
2023-07-03 19:02:05 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if (!audio) {
+    return;
+  }
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) =>
+  key.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== "transform") {
+      return;
+    }
+
+    key.classList.remove("playing");
+  })
+);
